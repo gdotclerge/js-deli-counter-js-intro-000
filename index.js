f4cf06064dedb77107509b3a7366430ba552eabe
Garry Clerge <clergeg@gmail.com>
2017-09-13 22:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,30 +10,33 @@ function takeANumber(katzDeliLine,name){
 function nowServing (katzDeliLine){
   if
     (katzDeliLine.length > 0 ){
-
     var serving = katzDeliLine.slice(0,1);
-
     katzDeliLine.shift();
 
     return `Currently serving ${serving}.`;
 
-  } else {return "There is nobody waiting to be served!"}
-
+  } else {return "There is nobody waiting to be served!"
+    }
 }
 
 
 
 
 function currentLine(katzDeliLine){
-
   if (katzDeliLine.length > 0) {
-   for (let i = 0; i < katzDeliLine.length; i++) {
-        var linenow = [];
-        linenow.push(`${i + 1}. ${katzDeliLine[i]}`);
+    var linenow = '';
+
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      
+      if (katzDeliLine[katzDeliLine.length - 1] === katzDeliLine[i]){
+          linenow += ` ${i + 1}. ${katzDeliLine[i]}`;
+      } else { linenow += ` ${i + 1}. ${katzDeliLine[i]},`;
+        }
     }
-
-  return `The line is currently: ${linenow}`;
-
+    
+    return `The line is currently: ${linenow}`;
+  
   } else {return "The line is currently empty.";
     }
 }
+
