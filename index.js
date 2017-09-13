226f65d5588df007ef69edacf87aedd13ce3a661
Garry Clerge <clergeg@gmail.com>
2017-09-13 20:36:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function takeANumber(katzDeliLine, name)[
+  
+]
+
+
+function nowServing (katzDeliLine){
+  if 
+    (katzDeliLine.length > 0 ){
+      
+      var serving = katzDeliLine.slice(0,1);
+      katzDeliLine.shift;
+      return `Currently serving ${serving}`;
+      
+  } else {return "There is no one wating to be served"}
+  
+}
