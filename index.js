63493f6c9f9cab4b9d13dbabcf3f5903c4bfff98
Garry Clerge <clergeg@gmail.com>
2017-09-13 21:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ function nowServing (katzDeliLine){
   } else {return "There is nobody waiting to be served!"}
 
 }
+
+
+
+
+function currentLine(katzDeliLine){
+
+  if (katzDeliLine.length > 0) {
+   for (let i = 0; i < katzDeliLine.length; i++) {
+        var linenow = [];
+        linenow.push(`${i + 1}. ${katzDeliLine[i]}`);
+    }
+    
+  return `The line is currently: ${linenow}`;
+  
+  } else {return "The line is currently empty.";
+    }
+}
+
+
