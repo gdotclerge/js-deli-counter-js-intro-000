ca8334836ea65fdb7a5ec13600eace2d5e2eab68
Garry Clerge <clergeg@gmail.com>
2017-09-13 21:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ function nowServing (katzDeliLine){
   if
     (katzDeliLine.length > 0 ){
 
-      var serving = katzDeliLine.slice(0,1);
-      katzDeliLine.shift();
-      return `Currently serving ${serving}.`;
+    var serving = katzDeliLine.slice(0,1);
+
+    katzDeliLine.shift();
+
+    return `Currently serving ${serving}.`;
 
   } else {return "There is nobody waiting to be served!"}
 
