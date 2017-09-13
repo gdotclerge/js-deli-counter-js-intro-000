b5c8a4e41445d73f4929943833640b038a878824
Garry Clerge <clergeg@gmail.com>
2017-09-13 20:55:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name); 
+
+  `Welcome, ${name}. You are number ${katzDeliLine.length} in line!`;
+}
+
+
 
 
 function nowServing (katzDeliLine){
